2d62e4c273fd3955950a6bbee18f604ec0d51101
Hasnain Khan <hk4547124@gmail.com>
2026-01-05 19:44:33 +0500

    Your commit message

--- nums-Maths.js
+++ nums-Maths.js
@@ -2,3 +2,8 @@ let score = 399
 
 const balance = new Number(199)
 console.log(balance);
+console.log(score.length); // length not work in numbers ONly on Arrays an Objects
+
+console.log(balance.toString())
+console.log(typeof balance.toString())
+console.log(typeof balance)
