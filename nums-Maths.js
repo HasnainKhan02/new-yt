f3dc51c71ef17e371ff3573b8d0fc99f1ca9b1c7
Hasnain Khan <hk4547124@gmail.com>
2026-01-05 20:01:42 +0500

    new Updates

--- nums-Maths.js
+++ nums-Maths.js
@@ -1,9 +1,31 @@
-let score = 399
+// let score = 399
 
-const balance = new Number(199)
-console.log(balance);
-console.log(score.length); // length not work in numbers ONly on Arrays an Objects
+// const balance = new Number(199)
+// console.log(balance);
+// console.log(score.length); // length not work in numbers ONly on Arrays an Objects
 
-console.log(balance.toString())
-console.log(typeof balance.toString())
-console.log(typeof balance)
+// console.log(balance.toString())
+// console.log(typeof balance.toString())
+// console.log(typeof balance)
+
+
+//  ********************** toFixed ***********************
+
+// let price = 232.342210
+
+// console.log(price.toFixed(2));
+// // OUT_PUT is   232.34
+
+//  Give those values which is right side of . DOT
+
+// let cost = 1312
+// console.log(cost.toFixed(2));
+// console.log(typeof cost.toFixed(2));
+
+// // OUT_PUT is   1312.00
+//  Give those values which is right side of . DOT => there is not values SO it Gives 00
+
+balance = 231211
+let rounded = Number(balance.toFixed(2));
+console.log(rounded); // 199.46
+
