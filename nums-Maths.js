d1b80ab6988b0ab7c8da123781dbc71607455efa
Hasnain Khan <hk4547124@gmail.com>
2026-01-06 14:33:27 +0500

    More-Practice

--- nums-Maths.js
+++ nums-Maths.js
@@ -40,5 +40,31 @@ console.log(typeof cost.toFixed(2));
 
 //  ********************** toLocalString() ***********************
 
-let hundered = 100000
-console.log(hundered.toLocaleString('en-PK'));
+// let hundered = 100000
+// console.log(hundered.toLocaleString('en-PK')); //  100,000 Add Commas ,, For understand more batter
+
+
+//  ********************** parseInt() ***********************
+
+
+// let number = "12345.23"
+//console.log(parseInt(number)); // ParseInt => Convert string to Numbers Also remove flot numbers on Right side like => .23 
+
+// let name = "Hasnian" 
+//console.log(parseInt(name)); //Output = "NaN"  => String is Converted to Numbers also Mean Name is not Number not String
+//console.log(typeof parseInt(name)); // Output = "number"
+
+
+//  ********************** parseFloat() ***********************
+// parseFloat() string ko decimal number (floating number) mein convert karta hai.
+let Brand_name = "Service"
+console.log(parseFloat(Brand_name)); // Output = "NaN"
+
+let Score = "23432.232"
+console.log(parseFloat(Score)) // Convert String to Float Number Add also Right Number after .Dot like => 123.3212 
+
+let Score2 = "23432.23kg"  // remove after the numbes string like kg removed 
+console.log(parseFloat(Score2))
+
+let Score3 = "kg23432.23"  
+console.log(parseFloat(Score3)) // Output => NaN Because of Kg use before numbers
