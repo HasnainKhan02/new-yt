065b4418b3d12717a9647265682aacbb895b8f95
Hasnain Khan <hk4547124@gmail.com>
2026-01-07 16:16:10 +0500

    new

---
+++ 02_Objects_Destructuring.js
@@ -0,0 +1,23 @@
+// ARRAYS DESTRUCTURING
+
+let course = {
+  name: "Biology",
+  teacher: "Ali",
+  classTime: "9am",
+
+};
+
+const {name , teacher , classTime} = course  // stored in variables
+
+
+console.log(teacher , classTime);
+
+
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$ JSON FORMAT $$$$$$$$$$$$$$$$$$$$$$$
+
+{
+  "name": "huzaifa" ,
+  "Age" : "31",
+  "Edu" : "BS"
+}
