4fb1012a7cbf203810c30e57fdbd0ca6ce040fab
Hasnain Khan <hk4547124@gmail.com>
2026-01-07 15:48:15 +0500

    some more object Methods

--- ObjectsInJS.js
+++ ObjectsInJS.js
@@ -31,28 +31,48 @@ function greeting() {
 //   name: "Ali",
 //   fullName: {
 //     firstName: "Huzaifa",
-//     lastName: "Khan",
-//   },
+// //     lastName: "Khan",
+// //   },
+// // };
+// // console.log(userName.fullName.lastName);
+
+
+// // $$$$$$$$$$$$$$$$$$$$  MERGE OBJECTS  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+// let obj1 = {
+//   a: "1",
+//   b: "2",
+//   c: "3",
+//   d: "4",
 // };
-// console.log(userName.fullName.lastName);
+// let obj2 = {
+//     e: "5",
+//     f: "6",
+//     g: "7",
+//     h: "8",
+// };
+
+// // let obj3 = Object.assign({} , obj1 , obj2)  // Merge Method
+// let obj3 = {...obj1 , ...obj2} // Spread Operator (Same for Array & Objects)
+// console.log(obj3);
 
 
-// $$$$$$$$$$$$$$$$$$$$  MERGE OBJECTS  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 
-let obj1 = {
-  a: "1",
-  b: "2",
-  c: "3",
-  d: "4",
+
+
+let Tender = {
+  name: "Hasnain",
+  age: 21,
+  email: "Hasnain@google=.com",
+  isOnline: true,
 };
-let obj2 = {
-  e: "5",
-  f: "6",
-  g: "7",
-  h: "8",
-};
+// ^^^^^^^^^^^^^^%%%%%%%%%%%%%  VERY IMPORTANT &&&&&&&&&&&&&&&&&&^^^^^^^^^^^^^
 
-let obj3 = Object.assign({} , obj1 , obj2)
-console.log(obj3);
+// console.log(Object.keys(Tender)); // give you Only Keys
+// console.log(Object.values(Tender)); // give you Only Values
+// console.log(Object.entries(Tender));  // give you keys and values in Seperate Array
 
+
+console.log(Tender.hasOwnProperty('email')) //Check  is it Avilable or NOt on Object
